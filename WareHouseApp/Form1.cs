7422c8573d2d692496069fd24df463dfd6ed760d
Onitha <onithahettiarachchi26@gmail.com>
2025-09-26 23:26:24 +0530

    Initial commit: Add WinForms project

---
+++ WareHouseApp/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WareHouseApp.People;
+using WareHouseApp.Resources.AppStrings;
+
+namespace WareHouseApp
+{
+    public partial class Form1 : Form
+    {
+        Admin admin = new Admin();
+        LoginPage loginPage = new LoginPage();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void butLogin_Click(object sender, EventArgs e)
+        {
+            string UserNameTxt = txtName.Text;
+            string PassTxt = txtPassword.Text;
+            try
+            {
+                admin.Login(UserNameTxt, PassTxt);
+                Console.WriteLine("Logged IN");
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                MessageBox.Show(loginPage.LoginErrorTitleEn, loginPage.LoginErrorMessageEn, MessageBoxButtons.RetryCancel);
+            }
+            finally
+            {
+                txtPassword.Text="";
+            }
+
+            
+        }
+    }
+}
---
+++ WareHouseApp/People/ShippingOperator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareHouseApp.People
+{
+    public class ShippingOperator : Person
+    {
+        
+        public override bool Login(string username, string password)
+        {
+            return false;
+        }
+        public override void ChangePassword(string newPassword)
+        {
+            this.Password = newPassword;
+        }
+        public override void Logout(int userID)
+        {
+
+        }
+
+        public int LoadStocks()
+        {
+            int stocks = 0;
+            return stocks;
+        }
+        public int ShipStocks()
+        {
+            int stocks = 0;
+            return stocks;
+        }
+    }
+}
